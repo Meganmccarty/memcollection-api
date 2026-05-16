0530ac6b817f736ed61f83e105655cb3e09666a5
Megan McCarty <megan_mccarty@hotmail.com>
2026-05-15 20:13:47 -0400

    Bump version in docs/source/conf.py, memcollection/settings/base.py, and package.json

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ django.setup()
 project = "MEM Collection API"
 copyright = "2026, Megan McCarty"
 author = "Megan McCarty"
-release = "0.10.2"
+release = "0.11.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
